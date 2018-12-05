1844e0cd59a1f67d3af52f787bea47769dd24f93
victoriahu <vh12490@gmail.com>
2018-12-04 17:00:03 -0800

    add callbackreview tests

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,39 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
+  
+// require('fs').readFileSync('abc.txt').toString().split('\n').forEach(function (line) { line; })
+  console.log(filePath);
+  fs.readFile(filePath, (err, fileData) => {
+    if (err) {
+      callback(err);
+    } else {
+      var firstLine = fileData.toString().split('\n')[0];
+      callback(null, firstLine);
+    }
+  });
 };
 
+// request("http://www.sitepoint.com", function(error, response, body) {
+//   console.log(body);
+// });
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = function (url, callback) {
   // TODO
+  request(url, (error, response) => {
+    // console.log(response.statusCode);
+    if (error) {
+      // console.log("error is running", error);
+      callback(new Error('Invalid URI')); 
+      // callback(error);
+      // callback(error, null);s
+    } else {
+      // console.log("status Code: ", response.statusCode);
+      callback(null, response.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
@@ -20,3 +46,25 @@ module.exports = {
   getStatusCode: getStatusCode,
   pluckFirstLineFromFile: pluckFirstLineFromFile
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
