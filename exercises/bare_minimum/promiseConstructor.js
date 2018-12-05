69698f41f81c6ea2e06d8880bf893cb46198395d
victoriahu <vh12490@gmail.com>
2018-12-04 17:15:41 -0800

    add pluckFirstLineFromFileAsync tests

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -8,9 +8,22 @@ var fs = require('fs');
 var request = require('request');
 var Promise = require('bluebird');
 
+
+
+
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
   // TODO
+  return new Promise(function(resolve, reject) {
+    fs.readFile(filePath, (err, fileData) => {
+      if (err) {
+        reject(err);
+      } else {
+        var firstLine = fileData.toString().split('\n')[0];
+        resolve(firstLine);
+      }
+    });
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
@@ -23,3 +36,15 @@ module.exports = {
   getStatusCodeAsync: getStatusCodeAsync,
   pluckFirstLineFromFileAsync: pluckFirstLineFromFileAsync
 };
+
+
+// There are five steps to writing a promise-returning function:
+
+// Create a promise with the new Promise constructor
+// Do something async, then...
+// Pass the successful value into the resolve function
+// this value will be made available in the next then block
+// only 1 value can ever be passed into resolve
+// Pass any errors into the reject function
+// this error will be made available in the catch block
+// return the promise instance. This should be a synchronous step
